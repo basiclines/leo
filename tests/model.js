7f18621167f41d736491f8155207b3b9c457c720
basiclines <igonzaleznicolas@gmail.com>
2017-11-05 22:12:16 +0100

    Added new tests for on/off

--- tests/model.js
+++ tests/model.js
@@ -1,4 +1,4 @@
-import assert from 'assert'
+import assert from 'assert' // Node assert built-in library
 import Model from 'src/model'
 
 describe('Model', function() {
@@ -22,7 +22,43 @@ describe('Model', function() {
 	describe('.clear()', function() {
 		it('value should be undefined', function() {
 				TestModel.clear()
-				assert.equal(typeof TestModel.value,  'undefined')
+				assert.equal(typeof TestModel.value, 'undefined')
+		})
+
+		it('value shouldn\'t be in the attributes hash', function() {
+				TestModel.clear()
+				assert.equal(typeof TestModel.attributes.value, 'undefined')
+		})
+	})
+
+	describe('.on()', function() {
+		it('callback should be fired', function(done) {
+				TestModel.on('change:value', function() {
+					done()
+				})
+				TestModel.value = value
+		})
+	})
+
+	describe('.off()', function() {
+		it('callback should be removed for specific event', function() {
+				TestModel.off('change:value')
+				assert.equal(TestModel.listeners, 0)
+		})
+
+		it('callback should be removed for specific event and handler', function() {
+				var func = () => { assert.fail('callback is fired for value1') }
+				TestModel.on('change:value1', func)
+				TestModel.off('change:value1', func)
+				TestModel.value1 = value
+		})
+
+		it('all callbacks should be removed', function() {
+				TestModel.on('change:value1', () => { assert.fail('callback is fired for value1') })
+				TestModel.on('change:value2', () => { assert.fail('callback is fired for value2') })
+				TestModel.off()
+				TestModel.value1 = value
+				TestModel.value2 = value
 		})
 	})
 
