254608f704206957328f8b6197b8f5923905a9d2
basiclines <igonzaleznicolas@gmail.com>
2017-12-10 16:27:34 +0100

    Isolated unit test

--- tests/unit/object_tests.js
+++ tests/unit/object_tests.js
@@ -3,72 +3,86 @@ import LEOObject from 'src/object'
 
 describe('LEOObject', function() {
 
-	const value = 'value'
-	let TestModel = new LEOObject()
-	let ObservableModel = new LEOObject()
-
+	const STRING_VALUE = 'value'
 
 	describe('set/get value', function() {
 		it('value should be assigned', function() {
-				TestModel.value = value
-				assert.equal(TestModel.value, value)
+			let TestModel = new LEOObject()
+			TestModel.value = STRING_VALUE
+			assert.equal(TestModel.value, STRING_VALUE)
 		})
 	})
 
 	describe('.attributes', function() {
 		it('value should be in the attributes hash', function() {
-				assert.equal(TestModel.attributes.value, value)
+			let TestModel = new LEOObject()
+			TestModel.value = STRING_VALUE
+			assert.equal(TestModel.attributes.value, STRING_VALUE)
 		})
 	})
 
 	describe('.clear()', function() {
+		let TestModel = new LEOObject()
+		TestModel.value = STRING_VALUE
+		TestModel.clear()
+
 		it('value should be undefined', function() {
-				TestModel.clear()
-				assert.equal(typeof TestModel.value, 'undefined')
+			assert.equal(typeof TestModel.value, 'undefined')
 		})
 
 		it('value shouldn\'t be in the attributes hash', function() {
-				TestModel.clear()
-				assert.equal(typeof TestModel.attributes.value, 'undefined')
+			assert.equal(typeof TestModel.attributes.value, 'undefined')
 		})
 	})
 
 	describe('.on()', function() {
 		it('callback should be fired', function(done) {
-				TestModel.on('change:value', function() {
-					done()
-				})
-				TestModel.value = value
+			let TestModel = new LEOObject()
+			TestModel.on('change:value', function() {
+				done()
+			})
+			TestModel.value = STRING_VALUE
 		})
 	})
 
 	describe('.off()', function() {
-		it('callback should be removed for specific event', function() {
-				TestModel.off('change:value')
-				assert.equal(TestModel.listeners, 0)
+		it('callback should be removed for specific event', function(done) {
+			let TestModel = new LEOObject()
+			TestModel.on('change:value', function() {
+				assert.fail('callback is fired for value')
+			})
+			TestModel.off('change:value')
+			TestModel.value = STRING_VALUE
+			done()
 		})
 
-		it('callback should be removed for specific event and handler', function() {
-				var func = () => { assert.fail('callback is fired for value1') }
-				TestModel.on('change:value1', func)
-				TestModel.off('change:value1', func)
-				TestModel.value1 = value
+		it('callback should be removed for specific event and handler', function(done) {
+			let TestModel = new LEOObject()
+			let handler = () => { assert.fail('callback is fired for value') }
+			TestModel.on('change:value', handler)
+			TestModel.off('change:value', handler)
+			TestModel.value = STRING_VALUE
+			done()
 		})
 
-		it('all callbacks should be removed', function() {
-				TestModel.on('change:value1', () => { assert.fail('callback is fired for value1') })
-				TestModel.on('change:value2', () => { assert.fail('callback is fired for value2') })
-				TestModel.off()
-				TestModel.value1 = value
-				TestModel.value2 = value
+		it('all callbacks should be removed', function(done) {
+			let TestModel = new LEOObject()
+			TestModel.on('change:value1', () => { assert.fail('callback is fired for value1') })
+			TestModel.on('change:value2', () => { assert.fail('callback is fired for value2') })
+			TestModel.off()
+			TestModel.value1 = STRING_VALUE
+			TestModel.value2 = STRING_VALUE
+			done()
 		})
 	})
 
 
 	describe('.isEmpty', function() {
+		let TestModel = new LEOObject()
+		TestModel.value = STRING_VALUE
+
 		it('should return false', function() {
-				TestModel.value = value
-				assert.equal(TestModel.isEmpty, false)
+			assert.equal(TestModel.isEmpty, false)
 		})
 
 		it('should return true', function() {
@@ -77,64 +91,70 @@ describe('LEOObject', function() {
 		})
 	})
 
-	describe('.listenTo', function() {
+	describe('.listenTo()', function() {
 		it('callback should be fired', function(done) {
+			let TestModel = new LEOObject()
+			let ObservableModel = new LEOObject()
 			TestModel.listenTo(ObservableModel, 'change:value', function(value) {
 				done()
 			})
-			ObservableModel.value = value;
+			ObservableModel.value = STRING_VALUE
 		})
 	})
 
-	describe('.stopListening', function() {
+	describe('.stopListening()', function() {
 		it('callback should be removed only for specific event', function(done) {
+			let TestModel = new LEOObject()
+			let ObservableModel = new LEOObject()
 			TestModel.listenTo(ObservableModel, 'change:value1', function() {
 				assert.fail('Callback is fired for value1')
 			})
-
 			TestModel.listenTo(ObservableModel, 'change:value2', function() {
 				done()
 			})
 
 			TestModel.stopListening(ObservableModel, 'change:value1')
-			ObservableModel.value1 = value
-			ObservableModel.value2 = value
+			ObservableModel.value1 = STRING_VALUE
+			ObservableModel.value2 = STRING_VALUE
 		})
 
 		it('callback should be removed only for specific event and handler', function(done) {
-				let LocalObservableModel = new LEOObject()
-				var func = () => { assert.fail('callback is fired for value') }
+			let TestModel = new LEOObject()
+			let ObservableModel = new LEOObject()
+			var func = () => { assert.fail('callback is fired for value') }
 
-				TestModel.listenTo(LocalObservableModel, 'change:value', func)
-				TestModel.listenTo(LocalObservableModel, 'change:value', function() { done() })
-				TestModel.stopListening(LocalObservableModel, 'change:value', func)
+			TestModel.listenTo(ObservableModel, 'change:value', func)
+			TestModel.listenTo(ObservableModel, 'change:value', function() { done() })
+			TestModel.stopListening(ObservableModel, 'change:value', func)
 
-				LocalObservableModel.value = value
+			ObservableModel.value = STRING_VALUE
 		})
 
 		it('all callbacks should be removed for specific model', function(done) {
-				let LocalObservableModel = new LEOObject()
+			let TestModel = new LEOObject()
+			let ObservableModel = new LEOObject()
 
-				TestModel.listenTo(LocalObservableModel, 'change:value1', () => { assert.fail('callback is fired for value1') })
-				TestModel.listenTo(LocalObservableModel, 'change:value2', () => { assert.fail('callback is fired for value2') })
-				TestModel.stopListening(LocalObservableModel)
+			TestModel.listenTo(ObservableModel, 'change:value1', () => { assert.fail('callback is fired for value1') })
+			TestModel.listenTo(ObservableModel, 'change:value2', () => { assert.fail('callback is fired for value2') })
+			TestModel.stopListening(ObservableModel)
 
-				LocalObservableModel.value1 = value
-				LocalObservableModel.value2 = value
-				done()
+			ObservableModel.value1 = STRING_VALUE
+			ObservableModel.value2 = STRING_VALUE
+			done()
 		})
 
 		it('all callbacks should be removed for all models', function(done) {
-				let LocalObservableModel = new LEOObject()
-				let LocalObservableModel2 = new LEOObject()
+			let TestModel = new LEOObject()
+			let ObservableModel1 = new LEOObject()
+			let ObservableModel2 = new LEOObject()
 
-				TestModel.listenTo(LocalObservableModel, 'change:value1', () => { assert.fail('callback is fired for LocalObservableModel') })
-				TestModel.listenTo(LocalObservableModel2, 'change:value1', () => { assert.fail('callback is fired for LocalObservableModel2') })
-				TestModel.stopListening()
+			TestModel.listenTo(ObservableModel1, 'change:value', () => { assert.fail('callback is fired for ObservableModel1') })
+			TestModel.listenTo(ObservableModel2, 'change:value', () => { assert.fail('callback is fired for ObservableModel2') })
+			TestModel.stopListening()
 
-				LocalObservableModel.value1 = value
-				LocalObservableModel2.value1 = value
-				done()
+			ObservableModel1.value = STRING_VALUE
+			ObservableModel2.value = STRING_VALUE
+			done()
 		})
 
 	})
